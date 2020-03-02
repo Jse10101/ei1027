1ee9fb72422f54e0336e4c7292ee8dbccce234a3
Jose <al270748@uji.es>
2020-03-02 19:48:03 +0100

    Primera subida

---
+++ src/main/java/es/uji/ei1027/majorsacasa/MajorsacasaApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.majorsacasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajorsacasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorsacasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uji/ei1027/majorsacasa/MajorsacasaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.majorsacasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorsacasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
