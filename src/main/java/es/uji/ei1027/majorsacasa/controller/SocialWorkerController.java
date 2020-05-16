0d81357f63a3ff5482bccf815244e9fd0d91874c
RMesado <rafaelmesado@gmail.com>
2020-05-16 19:58:39 +0200

    Creados menús para el SocialWorker/Admin, añadidos metodos en RequestDao y VolunteerDao, añadidos constructores en Volunteer y SocialWorker.

--- src/main/java/es/uji/ei1027/majorsacasa/controller/SocialWorkerController.java
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/SocialWorkerController.java
@@ -1,5 +1,8 @@
 package es.uji.ei1027.majorsacasa.controller;
 
+import es.uji.ei1027.majorsacasa.dao.*;
+import es.uji.ei1027.majorsacasa.model.Login;
+import es.uji.ei1027.majorsacasa.model.SocialWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,71 +12,248 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import es.uji.ei1027.majorsacasa.dao.SocialWorkerDao;
-import es.uji.ei1027.majorsacasa.model.SocialWorker;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/socialworker")
 public class SocialWorkerController {
 
-	   private SocialWorkerDao socialWorkerDao;
+    private SocialWorkerDao socialWorkerDao;
+    private RequestDao requestDao;
+    private ElderlyDao elderlyDao;
+    private CompanyDao companyDao;
+    private VolunteerDao volunteerDao;
 
-	   @Autowired
-	   public void SocialWorkerDao(SocialWorkerDao socialWorkerDao) {
-	       this.socialWorkerDao=socialWorkerDao;
-	   }
+    @Autowired
+    public void SocialWorkerDao(SocialWorkerDao socialWorkerDao) {
+        this.socialWorkerDao = socialWorkerDao;
+    }
 
-	   // Operacions: Crear, llistar, actualitzar, esborrar
-	   // ...
-	   @RequestMapping("/list")
-	   public String listSocialWorkers(Model model) {
-	      model.addAttribute("socialworkers", socialWorkerDao.getSocialWorkers());
-	      return "socialworker/list";
-	   }
-	   
-	   @RequestMapping(value="/add") 
-	   public String addSocialWorker(Model model) {
-	       model.addAttribute("socialworker", new SocialWorker());
-	       return "socialworker/add";
-	   }
-	   
-	   @RequestMapping(value="/add", method=RequestMethod.POST)
-	   public String processAddSubmit(@ModelAttribute("socialworker") SocialWorker socialWorker,
-	                                   BindingResult bindingResult) {
-		   SocialWorkerValidator socialworkerValidador = new SocialWorkerValidator();
-		   socialworkerValidador.validate(socialWorker, bindingResult);
-	        if (bindingResult.hasErrors())
-	               return "socialworker/add";
-	        socialWorkerDao.addSocialWorker(socialWorker);
-	        return "redirect:list";
-	    }
-	   
-	   @RequestMapping(value="/update/{userCAS}", method = RequestMethod.GET)
-	   public String editaSocialWorker(Model model, @PathVariable String userCAS) {
-	       model.addAttribute("socialworker", socialWorkerDao.getSocialWorker(userCAS));
-	       return "socialworker/update"; 
-	   }
+    @Autowired
+    public void setRequestDao(RequestDao requestDao) {
+        this.requestDao = requestDao;
+    }
 
-	   @RequestMapping(value="/update", method = RequestMethod.POST) 
-	   public String processUpdateSubmit(
-	                           @ModelAttribute("socialworker") SocialWorker socialWorker, 
-	                           BindingResult bindingResult) {
-		   SocialWorkerValidator socialworkerValidador = new SocialWorkerValidator();
-		   socialworkerValidador.validate(socialWorker, bindingResult);
-	        if (bindingResult.hasErrors()) 
-	            return "socialworker/update";
-	        socialWorkerDao.updateSocialWorker(socialWorker);
-	        return "redirect:list"; 
-	   }
+    @Autowired
+    public void setElderlyDao(ElderlyDao elderlyDao) {
+        this.elderlyDao = elderlyDao;
+    }
 
-	   @RequestMapping(value="/delete/{userCAS}")
-	   public String processDelete(@PathVariable String userCAS) {
-	          socialWorkerDao.deleteSocialWorker(userCAS);
-	          return "redirect:../list"; 
-	   }
+    @Autowired
+    public void setCompanyDao(CompanyDao companyDao) { this.companyDao = companyDao; }
 
-	   
-	
+    @Autowired
+    public void setVolunteerDao(VolunteerDao volunteerDao) { this.volunteerDao = volunteerDao; }
+
+    // Operacions: Crear, llistar, actualitzar, esborrar
+    // ...
+    @RequestMapping("/list")
+    public String listSocialWorkers(Model model) {
+        model.addAttribute("socialworkers", socialWorkerDao.getSocialWorkers());
+        return "socialworker/list";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addSocialWorker(Model model) {
+        model.addAttribute("socialworker", new SocialWorker());
+        return "socialworker/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("socialworker") SocialWorker socialWorker,
+                                   BindingResult bindingResult) {
+        SocialWorkerValidator socialworkerValidador = new SocialWorkerValidator();
+        socialworkerValidador.validate(socialWorker, bindingResult);
+        if (bindingResult.hasErrors())
+            return "socialworker/add";
+        socialWorkerDao.addSocialWorker(socialWorker);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/update/{userCAS}", method = RequestMethod.GET)
+    public String editaSocialWorker(Model model, @PathVariable String userCAS) {
+        model.addAttribute("socialworker", socialWorkerDao.getSocialWorker(userCAS));
+        return "socialworker/update";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(
+            @ModelAttribute("socialworker") SocialWorker socialWorker,
+            BindingResult bindingResult) {
+        SocialWorkerValidator socialworkerValidador = new SocialWorkerValidator();
+        socialworkerValidador.validate(socialWorker, bindingResult);
+        if (bindingResult.hasErrors())
+            return "socialworker/update";
+        socialWorkerDao.updateSocialWorker(socialWorker);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/delete/{userCAS}")
+    public String processDelete(@PathVariable String userCAS) {
+        socialWorkerDao.deleteSocialWorker(userCAS);
+        return "redirect:../list";
+    }
+
+    @RequestMapping("/home")
+    public String homeSocialworker(HttpSession session, Model model) {
+        Login login = (Login) session.getAttribute("login");
+
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/socialworker/home");
+            return "login";
+        }
+
+        SocialWorker socialWorker = new SocialWorker(socialWorkerDao.getSocialWorker(login.getUsuario()));
+        session.setAttribute("socialworker", socialWorker);
+        return "socialworker/home";
+
+    }
+
+    @RequestMapping("/menuElderly")
+    public String menuElderly(HttpSession session, Model model) {
+        Login login = (Login) session.getAttribute("login");
+
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/login");
+            return "login";
+        }
+
+
+        if (login.getRole().equals("socialworker")) {
+            model.addAttribute("requests", requestDao.getRequests());
+            model.addAttribute("elderlys", elderlyDao.getElderlys());
+            return "socialworker/menuElderly";
+        }
+
+        session.invalidate();
+        model.addAttribute("login", new Login());
+        session.setAttribute("nextUrl", "socialworker/menuElderly");
+        return "login";
+    }
+
+    @RequestMapping("/approveRequest/{idNumber}")
+    public String approveRequest(HttpSession session, Model model, @PathVariable String idNumber){
+        Login login = (Login) session.getAttribute("login");
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/login");
+            return "login";
+        }
+
+        if(login.getRole().equals("socialworker")){
+            requestDao.approveRequest(idNumber);
+            return "redirect:../menuElderly";
+        }
+        session.invalidate();
+        model.addAttribute("login", new Login());
+        session.setAttribute("nextUrl", "socialworker/menuElderly");
+        return "login";
+    }
+
+    @RequestMapping("/rejectRequest/{idNumber}")
+    public String rejectRequest(HttpSession session, Model model, @PathVariable String idNumber){
+        Login login = (Login) session.getAttribute("login");
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/login");
+            return "login";
+        }
+
+        if(login.getRole().equals("socialworker")){
+            requestDao.rejectRequest(idNumber);
+            return "redirect:../menuElderly";
+        }
+        session.invalidate();
+        model.addAttribute("login", new Login());
+        session.setAttribute("nextUrl", "socialworker/menuElderly");
+        return "login";
+    }
+
+    @RequestMapping("/menuCompany")
+    public String menuCompany(HttpSession session, Model model) {
+        Login login = (Login) session.getAttribute("login");
+
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/login");
+            return "login";
+        }
+
+
+        if (login.getRole().equals("socialworker")) {
+            model.addAttribute("companies", companyDao.getCompanies());
+            return "socialworker/menuCompany";
+        }
+
+        session.invalidate();
+        model.addAttribute("login", new Login());
+        session.setAttribute("nextUrl", "socialworker/menuCompany");
+        return "login";
+    }
+
+    @RequestMapping("/menuVolunteer")
+    public String menuVolunteer(HttpSession session, Model model) {
+        Login login = (Login) session.getAttribute("login");
+
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/login");
+            return "login";
+        }
+
+
+        if (login.getRole().equals("socialworker")) {
+            model.addAttribute("volunteers", volunteerDao.getVolunteers());
+            return "socialworker/menuVolunteer";
+        }
+
+        session.invalidate();
+        model.addAttribute("login", new Login());
+        session.setAttribute("nextUrl", "socialworker/menuVolunteer");
+        return "login";
+    }
+
+    @RequestMapping("/approveVolunteer/{dni}")
+    public String approveVolunteer(HttpSession session, Model model, @PathVariable String dni){
+        Login login = (Login) session.getAttribute("login");
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/login");
+            return "login";
+        }
+
+        if(login.getRole().equals("socialworker")){
+            volunteerDao.approveVolunteer(dni);
+            return "redirect:../menuVolunteer";
+        }
+        session.invalidate();
+        model.addAttribute("login", new Login());
+        session.setAttribute("nextUrl", "socialworker/menuVolunteer");
+        return "login";
+    }
+
+    @RequestMapping("/rejectVolunteer/{dni}")
+    public String rejectVolunteer(HttpSession session, Model model, @PathVariable String dni){
+        Login login = (Login) session.getAttribute("login");
+        if (login == null) {
+            model.addAttribute("login", new Login());
+            session.setAttribute("nextUrl", "/login");
+            return "login";
+        }
+
+        if(login.getRole().equals("socialworker")){
+            volunteerDao.rejectVolunteer(dni);
+            return "redirect:../menuVolunteer";
+        }
+        session.invalidate();
+        model.addAttribute("login", new Login());
+        session.setAttribute("nextUrl", "socialworker/menuVolunteer");
+        return "login";
+    }
+
+
 }
 
 
--- src/main/java/es/uji/ei1027/majorsacasa/dao/RequestDao.java
+++ src/main/java/es/uji/ei1027/majorsacasa/dao/RequestDao.java
@@ -1,31 +1,29 @@
 package es.uji.ei1027.majorsacasa.dao;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.DataSource;
-
+import es.uji.ei1027.majorsacasa.model.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import es.uji.ei1027.majorsacasa.model.Request;
+
+import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Repository
 public class RequestDao {
 
     private JdbcTemplate jdbcTemplate;
-    private int id=1;
-    
-    
-    
+    private int id = 1;
+
+
     @Autowired
     public void setDateSource(DataSource dataSource) {
-        jdbcTemplate=new JdbcTemplate(dataSource);
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
-    
+
 
     /* Afegeix la Request a la base de dades */
     public void addRequest(Request request) {
@@ -48,61 +46,69 @@ public class RequestDao {
         jdbcTemplate.update("UPDATE Request SET serviceType=?, creationDate=?, state=?, aprovedDate=?, rejectedDate=?, finishDate=?, comentari=?, dni_elderly=?, idNumber_contract=? WHERE idNumber=?",
                 request.getServiceType(), request.getCreationDate(), request.getState(), request.getAprovedDate(), request.getRejectedDate(), request.getFinishDate(), request.getComentario(), request.getDni_elderly(), null, request.getIdNumber());
     }
-    
+
     // Obté la Request amb el id donat. Torna null si no existeix. 
     public Request getRequest(String idNumber) {
         try {
             return jdbcTemplate.queryForObject("SELECT * FROM Request WHERE idNumber=?",
                     new RequestRowMapper(),
                     idNumber);
-        }
-        catch(EmptyResultDataAccessException e) {
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
     }
-        
+
 
     /* Obté totes les request. Torna una llista buida si no n'hi ha cap. */
     public List<Request> getRequests() {
         try {
             return jdbcTemplate.query("SELECT * FROM Request ORDER BY idNumber",
                     new RequestRowMapper());
-        }
-        catch(EmptyResultDataAccessException e) {
+        } catch (EmptyResultDataAccessException e) {
             return new ArrayList<Request>();
         }
     }
-    
-    
-    
+
+
     //NOVA REQUEST BAIXA
-    
+
     /* Actualitza els atributs de la Request
    (excepte la clau primària) */
     public void donaDeBaixaRequest(String idNumber) {
-    	LocalDate today = LocalDate.now();
+        LocalDate today = LocalDate.now();
         jdbcTemplate.update("UPDATE Request SET state=?, finishDate=? WHERE idNumber=?",
                 false, today, idNumber);
     }
-    
-    
-    
+
+
     //NOVA REQUEST ADD
-    
+
     /* Actualitza els atributs de la Request
    (excepte la clau primària) */
     public void donaDeAltaRequest(Request request) {
-    	LocalDate today = LocalDate.now();
-    	
-    	//Creamos un requestDao, hacemos una lista de Requests y la recorremos para comprobar el valor de id
-    	List<Request> listaRequest = this.getRequests();
-    	for(Request req : listaRequest) {
-    		if(Integer.valueOf(req.getIdNumber()) > id) {
-    			id= Integer.valueOf(req.getIdNumber());
-    		}
-    	}
-    	id++;
+        LocalDate today = LocalDate.now();
+
+        //Creamos un requestDao, hacemos una lista de Requests y la recorremos para comprobar el valor de id
+        List<Request> listaRequest = this.getRequests();
+        for (Request req : listaRequest) {
+            if (Integer.valueOf(req.getIdNumber()) > id) {
+                id = Integer.valueOf(req.getIdNumber());
+            }
+        }
+        id++;
         jdbcTemplate.update("INSERT INTO Request VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Integer.toString(id),
                 request.getServiceType(), today, false, null, null, null, request.getComentario(), request.getDni_elderly(), null);
+    }
+
+    public void approveRequest(String idNumber) {
+        LocalDate today = LocalDate.now();
+
+        jdbcTemplate.update("UPDATE Request SET state=?, aprovedDate=? WHERE idNumber=?", true, today, idNumber);
+    }
+
+    public void rejectRequest(String idNumber) {
+        LocalDate today = LocalDate.now();
+
+        jdbcTemplate.update("UPDATE Request SET state=?, rejectedDate=? WHERE idNumber=?", false, today, idNumber);
     }
 }
