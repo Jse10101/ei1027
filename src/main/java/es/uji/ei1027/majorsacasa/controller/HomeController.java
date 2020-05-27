2bc7a1872b1677fe671a9d33cf08ed6935271b97
Jose <al270748@uji.es>
2020-05-27 18:41:15 +0200

    Login en raiz

---
+++ src/main/java/es/uji/ei1027/majorsacasa/controller/HomeController.java
@@ -0,0 +1,16 @@
+package es.uji.ei1027.majorsacasa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+    @RequestMapping("/")
+    public String homeInit(Model model) {
+        return "redirect:/login"; 
+    }
+}
+
+
